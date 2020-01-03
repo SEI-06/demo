9799092e5031b74a1d378802a916d1cf3385588f
Derek <robinmurphy@Robins-MacBook-Pro.local>
2020-01-03 15:10:29 -0500

    Added Ajax Code
    Attempted to turn get to an index

--- assets/scripts/books/get.js
+++ assets/scripts/books/get.js
@@ -1,3 +1,14 @@
+'use strict'
 
-const app = require('../app.js')
-const config = require('../config.js')
+const api = require('./api')
+const ui = require('./ui')
+
+const onGetBooks = () => {
+  api.index()
+    .then(ui.getBooksSuccess) // return a sucessful results
+    .catch(ui.getBooksFailure) // return an unsucessful result
+}
+
+module.exports = {
+  onGetBooks
+}
